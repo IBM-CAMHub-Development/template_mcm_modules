1985427fafbc39d7d04dc0223c3969274298b2af
CAMHub PublishBot <octravis@us.ibm.com>
2019-10-23 12:48:40 +0000

    Automated CAMHub::publish on 2019-10-23 12:48:39 UTC

---
+++ cluster_credentials/main.tf
@@ -0,0 +1,46 @@
+## Set up local variables to be used
+locals {
+  kubeconfig_file      = "${var.work_directory}/target_cluster_kubeconfig.yaml"
+  certificate_file     = "${var.work_directory}/target_cluster_certificate.pem"
+  credentials_file     = "${var.work_directory}/cluster_credentials.json"
+}
+
+resource "local_file" "create_kubeconfig_file" {
+  count      = "${length(var.cluster_config) > 0 ? 1 : 0}"
+  content    = "${base64decode(var.cluster_config)}"
+  filename   = "${local.kubeconfig_file}"
+}
+
+resource "local_file" "create_certificate_file" {
+  count      = "${length(var.cluster_certificate_authority) > 0 ? 1 : 0}"
+  content    = "${var.cluster_certificate_authority}"
+  filename   = "${local.certificate_file}"
+}
+
+resource "null_resource" "generate-credentials" {
+  provisioner "local-exec" {
+    command = "chmod 755 ${path.module}/scripts/get_cluster_credentials.sh && ${path.module}/scripts/get_cluster_credentials.sh -ct ${var.cluster_type} -wd ${var.work_directory} -cf ${local.credentials_file}"
+    environment {
+      ## AKS, EKS, GKE, IKS
+      CLUSTER_NAME                = "${var.cluster_name}"
+      CLUSTER_CONFIG_FILE         = "${local.kubeconfig_file}"
+      ## IKS
+      CLUSTER_CA_CERTIFICATE_FILE = "${local.certificate_file}"
+      ## GKE
+      SERVICE_ACCOUNT_CREDENTIALS = "${var.service_account_credentials}"
+      ## EKS
+      ACCESS_KEY_ID               = "${var.access_key_id}"
+      SECRET_ACCESS_KEY           = "${var.secret_access_key}"
+      CLUSTER_REGION              = "${var.cluster_region}"
+      # ICP
+      ICP_URL                     = "${var.icp_url}"
+      ICP_ADMIN_USER              = "${var.icp_admin_user}"
+      ICP_ADMIN_PASSWORD          = "${var.icp_admin_password}"
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "mkdir -p ${var.work_directory} && jq -n --arg no_access 'noAccess' '{default:($$no_access)}' > ${local.credentials_file}"
+  }
+}
